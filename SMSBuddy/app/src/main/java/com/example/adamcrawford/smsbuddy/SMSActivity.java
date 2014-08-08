a3703d0a141230f16451c703b75105ad669cad31
Adam Crawford <geeksbsmrt@gmail.com>
2014-08-08 12:36:09 -0400

    Customize the messages list, need to make a custom adapter and figure out how to split the screen in half for incoming and outgoing messages.

--- SMSBuddy/app/src/main/java/com/example/adamcrawford/smsbuddy/SMSActivity.java
+++ SMSBuddy/app/src/main/java/com/example/adamcrawford/smsbuddy/SMSActivity.java
@@ -1,36 +1,44 @@
 package com.example.adamcrawford.smsbuddy;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import com.example.adamcrawford.smsbuddy.R;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class SMSActivity extends Activity {
+
+    private String TAG = "SMSA";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sms);
-    }
+        this.setTitle(R.string.newMessage);
 
+        EditText to = (EditText) findViewById(R.id.toEdit);
+        final EditText msg = (EditText) findViewById(R.id.messageEdit);
+        Button send = (Button) findViewById(R.id.sendButton);
+        ListView msgList = (ListView) findViewById(R.id.msgList);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.sm, menu);
-        return true;
-    }
+        Bundle intent = getIntent().getExtras();
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+        msg.setText(intent.getString(Intent.EXTRA_TEXT));
+
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!msg.getText().toString().equals("")) {
+                    Log.i(TAG, msg.getText().toString());
+
+                } else {
+                    Log.e(TAG, "Message blank");
+                }
+            }
+        });
+
     }
 }
