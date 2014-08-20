e2a465868449f875c40021693005a62c6ed32fa6
Adam Crawford <geeksbsmrt@gmail.com>
2014-08-20 17:47:02 -0400

    start coding detail activity

--- Socialite/app/src/main/java/com/example/adamcrawford/socialite/details/DetailActivity.java
+++ Socialite/app/src/main/java/com/example/adamcrawford/socialite/details/DetailActivity.java
@@ -2,6 +2,8 @@ package com.example.adamcrawford.socialite.details;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebView;
+
 import com.example.adamcrawford.socialite.R;
 
 public class DetailActivity extends Activity {
@@ -10,5 +12,9 @@ public class DetailActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        WebView webView = (WebView) findViewById(R.id.webView);
+
+
     }
 }
