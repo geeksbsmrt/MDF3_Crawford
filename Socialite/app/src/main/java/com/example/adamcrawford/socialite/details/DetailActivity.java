e68c4e63028c551a4d30b93cde5c2d4e03887c94
Adam Crawford <geeksbsmrt@gmail.com>
2014-08-19 22:02:02 -0400

    all data piped to list.

---
+++ Socialite/app/src/main/java/com/example/adamcrawford/socialite/details/DetailActivity.java
@@ -0,0 +1,14 @@
+package com.example.adamcrawford.socialite.details;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.example.adamcrawford.socialite.R;
+
+public class DetailActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
