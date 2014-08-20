94b77eb90e362adb68397bfb75e45c2d0a80d3fd
Adam Crawford <geeksbsmrt@gmail.com>
2014-08-19 21:09:34 -0400

    Sync Data

---
+++ Socialite/app/src/main/java/com/example/adamcrawford/socialite/Data/DataStorage.java
@@ -0,0 +1,11 @@
+package com.example.adamcrawford.socialite.data;
+
+/**
+ * Author:  Adam Crawford
+ * Project: Socialite
+ * Package: com.example.adamcrawford.socialite.data
+ * File:    DataStorage
+ * Purpose: TODO Minimum 2 sentence description
+ */
+public class DataStorage {
+}
---
+++ Socialite/app/src/main/java/com/example/adamcrawford/socialite/Data/SyncService.java
@@ -0,0 +1,11 @@
+package com.example.adamcrawford.socialite.Data;
+
+/**
+ * Author:  Adam Crawford
+ * Project: Socialite
+ * Package: com.example.adamcrawford.socialite.Data
+ * File:    SyncService
+ * Purpose: TODO Minimum 2 sentence description
+ */
+public class SyncService {
+}
