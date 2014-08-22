a706568f3ec9ae9302e54eb005f8faba8bb9a597
Adam Crawford <geeksbsmrt@gmail.com>
2014-08-21 23:56:47 -0400

    fix data persistence

--- Socialite/app/src/main/java/com/example/adamcrawford/socialite/EventWidgetProvider.java
+++ Socialite/app/src/main/java/com/example/adamcrawford/socialite/EventWidgetProvider.java
@@ -3,6 +3,7 @@ package com.example.adamcrawford.socialite;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -13,6 +14,7 @@ import android.util.Log;
 import android.widget.RemoteViews;
 
 import com.example.adamcrawford.socialite.dataHandler.DataStorage;
+import com.example.adamcrawford.socialite.events.EventConstructor;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -30,98 +32,19 @@ public class EventWidgetProvider extends AppWidgetProvider {
 
     SharedPreferences preferences;
     String TAG = "EWP";
-    static RemoteViews widgetView;
-    Intent viewIntent;
-    int widgetID;
 
     @Override
-    public void onReceive(Context context, Intent intent) {
-        super.onReceive(context, intent);
+    public void onEnabled(Context context) {
+        super.onEnabled(context);
 
-        widgetView = new RemoteViews(context.getPackageName(), R.layout.layout_widget);
-        viewIntent = null;
+        AppWidgetManager mgr = AppWidgetManager.getInstance(context);
 
+        // Get the layout for the App Widget
+        RemoteViews views = new RemoteViews(context.getPackageName(),R.layout.layout_widget);
 
-        Bundle extras = intent.getExtras();
+        ComponentName comp = new ComponentName(context.getPackageName(), EventWidgetProvider.class.getName());
 
-        if (extras != null) {
-            widgetID = extras.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
-
-            if (widgetID != AppWidgetManager.INVALID_APPWIDGET_ID) {
-                JSONObject events = null;
-
-                SharedPreferences.Editor edit = preferences.edit();
-
-                try {
-                    events = new JSONObject(DataStorage.getInstance().readFile("events", context));
-                } catch (JSONException e) {
-                    Log.e(TAG, e.getMessage());
-                }
-
-                if (events != null) {
-                    Log.i(TAG, events.toString());
-
-                    if (preferences.contains("currentEventNumber")){
-                        JSONObject currentEvent = null;
-                        int currentEventNumber = preferences.getInt("currentEvent", -1);
-
-                        if (currentEventNumber != -1) {
-                            Log.i(TAG, "Getting current event");
-                            try {
-                                currentEvent = events.getJSONArray("events").getJSONObject(currentEventNumber);
-                            } catch (JSONException e) {
-                                Log.e(TAG, e.getMessage());
-                            }
-                        }
-
-                        if (currentEvent != null){
-                            try {
-                                widgetView.setTextViewText(R.id.widgetEventName, currentEvent.getJSONObject("event").getString("title"));
-                                widgetView.setTextViewText(R.id.widgetEventTime, currentEvent.getJSONObject("event").getString("start_date"));
-                                widgetView.setTextViewText(R.id.widgetEventCity, currentEvent.getJSONObject("event").getString("city"));
-
-                                viewIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(currentEvent.getJSONObject("event").getString("url")));
-
-                            } catch (JSONException e) {
-                                Log.e(TAG, e.getMessage());
-                            }
-                        } else {
-                            Log.i(TAG, "Null current event");
-                            try {
-                                widgetView.setTextViewText(R.id.widgetEventName, events.getJSONArray("events").getJSONObject(1).getJSONObject("event").getString("title"));
-                                widgetView.setTextViewText(R.id.widgetEventTime, events.getJSONArray("events").getJSONObject(1).getJSONObject("event").getString("start_date"));
-                                widgetView.setTextViewText(R.id.widgetEventCity, events.getJSONArray("events").getJSONObject(1).getJSONObject("event").getJSONObject("venue").getString("city"));
-                                viewIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(events.getJSONArray("events").getJSONObject(1).getJSONObject("event").getString("url")));
-                            } catch (JSONException e) {
-                                Log.e(TAG, e.getMessage());
-                            }
-                            edit.putInt("currentEventNumber", 1);
-                            edit.apply();
-                        }
-                    } else {
-                        Log.i(TAG,"No current event");
-                        try {
-                            widgetView.setTextViewText(R.id.widgetEventName, events.getJSONArray("events").getJSONObject(1).getJSONObject("event").getString("title"));
-                            widgetView.setTextViewText(R.id.widgetEventTime, events.getJSONArray("events").getJSONObject(1).getJSONObject("event").getString("start_date"));
-                            widgetView.setTextViewText(R.id.widgetEventCity, events.getJSONArray("events").getJSONObject(1).getJSONObject("event").getJSONObject("venue").getString("city"));
-                            viewIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(events.getJSONArray("events").getJSONObject(1).getJSONObject("event").getString("url")));
-                            edit.putInt("currentEventNumber", 1);
-                            edit.apply();
-                        } catch (JSONException e) {
-                            Log.e(TAG, e.getMessage());
-                        }
-                    }
-                }
-
-                if (viewIntent != null) {
-                    Log.i(TAG, "Setting Pending Intent");
-                    PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, viewIntent, 0);
-                    widgetView.setOnClickPendingIntent(R.id.imgButton, pendingIntent);
-                }
-
-                AppWidgetManager.getInstance(context).updateAppWidget(widgetID, widgetView);
-            }
-        }
+        mgr.updateAppWidget(comp, views);
     }
 
     @Override
