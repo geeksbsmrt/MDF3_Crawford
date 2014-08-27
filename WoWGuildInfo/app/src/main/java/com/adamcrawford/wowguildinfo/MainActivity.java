26a3dc8512837e48ad93f244ca6ce1f4874a8e92
Adam Crawford <geeksbsmrt@gmail.com>
2014-08-26 22:14:58 -0400

    Webview loading

--- WoWGuildInfo/app/src/main/java/com/adamcrawford/wowguildinfo/MainActivity.java
+++ WoWGuildInfo/app/src/main/java/com/adamcrawford/wowguildinfo/MainActivity.java
@@ -2,6 +2,8 @@ package com.adamcrawford.wowguildinfo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 
 public class MainActivity extends Activity {
@@ -10,5 +12,11 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView webView = (WebView) findViewById(R.id.webView);
+        WebSettings webSettings = webView.getSettings();
+        webView.loadUrl("file:///android_asset/www/index.html");
+        webSettings.setJavaScriptEnabled(true);
+
     }
 }
