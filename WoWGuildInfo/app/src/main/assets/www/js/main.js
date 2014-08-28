ad4de457db510c62064a45adde8877daca1b9043
Adam Crawford <geeksbsmrt@gmail.com>
2014-08-28 18:20:14 -0400

    Hard coded two realms, picking project back up.  Working Web intent, launches device browser to view info about toon or guild

--- WoWGuildInfo/app/src/main/assets/www/js/main.js
+++ WoWGuildInfo/app/src/main/assets/www/js/main.js
@@ -1,10 +1,33 @@
-
-$(document).on('pagebeforeshow', "#home", function () {
-$('#realmName').append(new Option('Foo', 'foo', true, true));
-
-});
-
 $("#home").on('pageshow', function() {
-$('fieldset').hide();
-
+    var type;
+	$('fieldset').hide();
+	$("#lookupType").change(function () {
+	    var display = $("#lookupType option:selected").val();
+        if (display === "charLookup") {
+            type = "char";
+            $("#guildLookup").hide();
+            $("#charLookup").show();
+        } else if (display === "guildLookup") {
+            type = "guild";
+            $("#charLookup").hide();
+            $("#guildLookup").show();
+        } else {
+            //something is wrong
+        }
+	});
+	$("#submitButton").on('click', function(e){
+	    e.preventDefault();
+        var realm = $("#realmName").val(),
+            guild,
+            char;
+        if (type === "char") {
+            char = $("#cName").val();
+            Android.charLookup(realm, char);
+        } else if (type === "guild") {
+            guild = $("#gName").val();
+            Android.guildLookup(realm, guild);
+        } else {
+            //something wrong
+        }
+	});
 });
--- WoWGuildInfo/app/src/main/java/com/adamcrawford/wowguildinfo/MainActivity.java
+++ WoWGuildInfo/app/src/main/java/com/adamcrawford/wowguildinfo/MainActivity.java
@@ -3,8 +3,12 @@ package com.adamcrawford.wowguildinfo;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -20,6 +24,7 @@ public class MainActivity extends Activity {
         WebView webView = (WebView) findViewById(R.id.webView);
         WebSettings webSettings = webView.getSettings();
         webView.loadUrl("file:///android_asset/www/index.html");
+
         webView.addJavascriptInterface(new JavaScriptInterface(this), "Android");
         webSettings.setJavaScriptEnabled(true);
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN){
@@ -50,10 +55,27 @@ public class MainActivity extends Activity {
             mContext = c;
         }
 
-        //using Javascript to call the finish activity
-        public void closeMyActivity() {
-            finish();
+        @JavascriptInterface
+        public void logMessage(String string){
+            Log.i("Android", string);
         }
 
+        @JavascriptInterface
+        public void charLookup(String rName, String cName) {
+            Log.i("CharLookup", rName + " " +  cName);
+            String url = "http://us.battle.net/wow/en/character/" + rName.toLowerCase() + "/" + cName.toLowerCase() + "/simple";
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse(url));
+            startActivity(intent);
+        }
+
+        @JavascriptInterface
+        public void guildLookup(String rName, String gName) {
+            Log.i("GuildLookup", rName + " " + gName);
+            String url = "http://us.battle.net/wow/en/guild/" + rName.toLowerCase() + "/" + gName.toLowerCase().replace(" ", "_") + "/roster";
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse(url));
+            startActivity(intent);
+        }
     }
 }
