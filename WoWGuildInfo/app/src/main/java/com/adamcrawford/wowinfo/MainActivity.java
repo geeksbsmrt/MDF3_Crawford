7766b613e7309c505053f5afd9da4cedc3fa1bc5
Adam Crawford <geeksbsmrt@gmail.com>
2014-08-28 21:39:23 -0400

    fix issue on orientation change keeping JS from firing.

--- WoWGuildInfo/app/src/main/java/com/adamcrawford/wowinfo/MainActivity.java
+++ WoWGuildInfo/app/src/main/java/com/adamcrawford/wowinfo/MainActivity.java
@@ -13,38 +13,37 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import org.jetbrains.annotations.NotNull;
+
 
 public class MainActivity extends Activity {
+
+    WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        WebView webView = (WebView) findViewById(R.id.webView);
-        WebSettings webSettings = webView.getSettings();
-        webView.loadUrl("file:///android_asset/www/index.html");
-
+        webView = (WebView) findViewById(R.id.webView);
         webView.addJavascriptInterface(new JavaScriptInterface(this), "Android");
+        WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN){
             webSettings.setAllowUniversalAccessFromFileURLs(true);
             webSettings.setAllowFileAccessFromFileURLs(true);
         }
 
-        final ProgressDialog progressDialog = new ProgressDialog(this);
-        progressDialog.setMessage("Loading ...");
-        progressDialog.setCancelable(false);
-        progressDialog.show();
-
-        webView.setWebViewClient(new WebViewClient() {
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                super.onPageFinished(view, url);
-                progressDialog.hide();
-            }
-        });
-
+        if (savedInstanceState != null) {
+            webView.restoreState(savedInstanceState);
+        } else {
+            webView.loadUrl("file:///android_asset/www/index.html");
+        }
+    }
+    @Override
+    protected void onSaveInstanceState(@NotNull Bundle outState) {
+        webView.saveState(outState);
+        super.onSaveInstanceState(outState);
     }
 
     public class JavaScriptInterface {
